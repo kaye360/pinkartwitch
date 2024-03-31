5419339b2c94903247852c66a408ba229cb07bad
joshkaye88 <jkaye@shaw.ca>
2024-03-30 22:27:49 -0700

    get basic functionality of slider art slider working

--- src/lib/ArtModal.ts
+++ src/lib/ArtModal.ts
@@ -1,58 +1,47 @@
+import { imageList, type ArtImage } from './imageList';
 
 export default class ArtModal {
 
     dataModalElName : string
-    dataModalContentElName : string
     artBtnElName : string
-    closeModalBtnElName : string
 
     modal : HTMLDivElement | null
-    modalContent : HTMLDivElement | null
 
+    currentImageIndex : number | null
+    imageList : ArtImage[]
 
-    constructor({
-        modal, 
-        modalContent,
-        artBtn,
-        closeModalBtn
-    } : {
-        modal: string, 
-        modalContent: string
-        artBtn: string
-        closeModalBtn: string
-    }) {
+    constructor({ modal, artBtn } : { modal: string, artBtn: string }) {
         this.dataModalElName = modal
-        this.dataModalContentElName = modalContent
         this.artBtnElName = artBtn
-        this.closeModalBtnElName = closeModalBtn
         this.modal = null
-        this.modalContent = null
+        this.imageList = imageList
+        this.currentImageIndex = null
     }
 
     init = () => {
-        this.modal 	      = document.querySelector(this.dataModalElName) as HTMLDivElement
-        this.modalContent = document.querySelector(this.dataModalContentElName) as HTMLDivElement
+        this.modal = document.querySelector(this.dataModalElName) as HTMLDivElement
+        if(!this.modal) throw('Error: Modal Element not found')
 
-        if(!this.modal || !this.modalContent) throw('Error: Modal or ModalContent Element was not found')
-
-        const artBtnList = Array.from( document.querySelectorAll(this.artBtnElName) ) as HTMLButtonElement[]
-        artBtnList.forEach( () => addEventListener('click', this.openModal) )
-
-        const closeModalBtn = document.querySelector(this.closeModalBtnElName) as HTMLButtonElement
-        closeModalBtn.addEventListener('click', this.closeModal)
+        const artBtnList = document.querySelectorAll(this.artBtnElName) as NodeListOf<HTMLButtonElement>
+        artBtnList.forEach( (btn , i) => btn.addEventListener('click', (e : MouseEvent) => { this.openModal(i,e) }) )
     }
 
-    openModal = (e: MouseEvent) => {
+    openModal = (i: number, e: MouseEvent) => {
         if (! (e.target instanceof HTMLElement) ) return
-
         if( this.isModalOpen() ) return
 
         const image = this.getClickedImage(e.clientX, e.clientY)
         if( !image ) return
+        
+        this.currentImageIndex = i
 
-        const modalHTML = this.generateModalHTML(image)
+        const modalHTML  = 
+            this.generateCloseButtonHTML() +
+            this.generateScrollerHTML() +
+            this.generateImageHTML(image)
+
+            
         this.showModalUI(modalHTML)
-
         this.setIsModalOpen(true)
 
     }
@@ -62,36 +51,113 @@ export default class ArtModal {
         this.setIsModalOpen(false)
     }
 
-    generateModalHTML = (image : HTMLImageElement): string => {
+    generateCloseButtonHTML = () => {
+        return `
+            <div class="mx-auto w-full max-w-6xl flex justify-end px-2">
+                <button data-close-art-modal-button>
+                    Close
+                </button>
+            </div>
+        `
+    }
+    
+    generateScrollerHTML = () => {
+        return `
+            <div class="mx-auto w-full max-w-6xl" data-art-modal-scroller>
+                <div class="my-4 pb-2 mx-4 overflow-y-scroll flex justify-start gap-2 scrollbar">
+                    ${imageList.map( (img, i: number) => (
+                        `<button class="w-auto min-w-fit ${i !== this.currentImageIndex ? 'opacity-30' : ''}">
+                            <img 
+                                src="${img.src}"
+                                class="h-[100px] w-auto rounded"
+                            >
+                        </button>`
+                        ))}
+                </div>
+            </div>
+        `
+
+    }
+
+    generateImageHTML = (image : HTMLImageElement): string => {
         const {src, naturalWidth: width, naturalHeight: height} = image
-        const {title, description} = image.dataset
+        const {title, description, index} = image.dataset
 
         return `
-            <div class="art-modal-animation">
-                <img 
-                    src="${src}" 
-                    width="${width}" 
-                    height="${height}" 
-                    alt="" 
-                    class='rounded'
-                    style='height:50vh; width: auto'
-                >
-                <h2>${title}</h2>
-                <p>${description}</p>
+            <div data-art-modal-content class="grid place-items-center">
+                <div class="art-modal-animation">
+                    <img 
+                        src="${src}" 
+                        width="${width}" 
+                        height="${height}" 
+                        alt="" 
+                        class='rounded'
+                        style='height:50vh; width: auto'
+                        data-index="${index}"
+                    >
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </div>
             </div>
         `
     }
 
+    addScrollerEventListeners = () => {
+        const buttons = document.querySelector('[data-art-modal-scroller]')?.querySelectorAll('button') as NodeListOf<HTMLButtonElement>
+        buttons.forEach( (button, i) => { 
+            button.addEventListener('click', () => this.moveToImage(i) )
+        })
+    }
+
+    moveToImage = (i: number) => {
+        this.setScrollerOpacities(i)
+        this.swapImage(imageList[i])
+    }
+
+    setScrollerOpacities = (i:number) => {
+        const buttons = document.querySelector('[data-art-modal-scroller]')?.querySelectorAll('button') as NodeListOf<HTMLButtonElement>
+        buttons.forEach( (button, index) => { 
+            if( i === index) {
+                button.classList.remove('opacity-30')
+            } else {
+                button.classList.add('opacity-30')
+            }
+        })
+    }
+
+    getCurrentImage = () => {
+        return document.querySelector('[data-art-modal-content] img') as HTMLImageElement
+    }
+
+    swapImage(image: ArtImage) {
+
+        this.getCurrentImage().remove()
+
+        const newImage  = document.createElement('img')
+        newImage.src    = image.src
+        newImage.width  = image.width
+        newImage.height = image.height
+        newImage.setAttribute('class', 'rounded')
+        newImage.setAttribute('style', 'height:50vh; width: auto')
+
+        newImage.dataset.title = image.title
+        newImage.dataset.description = image.description
+
+        document.querySelector('[data-art-modal-content]>div')?.insertAdjacentElement('afterbegin', newImage)
+    }
+
     showModalUI = (modalHTML: string) => {
-        if(!this.modal || !this.modalContent) return
-        this.modalContent.insertAdjacentHTML('beforeend', modalHTML)
+        if( !this.modal ) return
+        this.modal.insertAdjacentHTML('beforeend', modalHTML)
         this.modal.classList.remove('opacity-0', '-z-50')
         this.modal.classList.add('z-[9999]')
+        document.querySelector('[data-close-art-modal-button]')?.addEventListener('click', this.closeModal)
+        this.addScrollerEventListeners()
     }
 
     hideModalUI = () => {
-        if(!this.modal || !this.modalContent) return
-        this.modalContent.textContent = ''
+        if( !this.modal ) return
+        this.modal.textContent = ''
         this.modal.classList.add('opacity-0', '-z-50')
         this.modal.classList.remove('z-[9999]')
     }
@@ -108,5 +174,18 @@ export default class ArtModal {
         const imgNodes = document.elementsFromPoint(x,y)
             .filter( el => el.tagName === 'IMG' ) as HTMLImageElement[]
         return imgNodes.length < 1 ? null : imgNodes[0]
+    }
+
+    mockOpenModal = () => {
+        // For Dev Use
+        this.currentImageIndex = 0
+        const image = document.querySelector('[data-art-button]>img') as HTMLImageElement
+        const modalHTML  = 
+            this.generateCloseButtonHTML() +
+            this.generateScrollerHTML() +
+            this.generateImageHTML(image)
+
+        this.showModalUI(modalHTML)
+        this.setIsModalOpen(true)
     }
 }
---
+++ src/lib/imageList.ts
@@ -0,0 +1,23 @@
+
+export interface ArtImage {
+    src : string,
+    width : number,
+    height : number,
+    title : string,
+    description : string
+}
+
+export const imageList : ArtImage[] = [
+    { src: '/art/art-3.webp', width : 827,  height : 1060, title : 'title 3', description : 'description 3' },
+    { src: '/art/art-1.webp', width : 800,  height : 1236, title : 'title 1', description : 'description 1' },
+    { src: '/art/art-2.webp', width : 800,  height : 800,  title : 'title 2', description : 'description 2' },
+    { src: '/art/art-3.webp', width : 827,  height : 1060, title : 'title 3', description : 'description 3' },
+    { src: '/art/art-4.webp', width : 1006, height : 1007, title : 'title 4', description : 'description 4' },
+    { src: '/art/art-1.webp', width : 800,  height : 1236, title : 'title 1', description : 'description 1' },
+    { src: '/art/art-2.webp', width : 800,  height : 800,  title : 'title 2', description : 'description 2' },
+    { src: '/art/art-4.webp', width : 1006, height : 1007, title : 'title 4', description : 'description 4' },
+    { src: '/art/art-1.webp', width : 800,  height : 1236, title : 'title 1', description : 'description 1' },
+    { src: '/art/art-2.webp', width : 800,  height : 800,  title : 'title 2', description : 'description 2' },
+    { src: '/art/art-3.webp', width : 827,  height : 1060, title : 'title 3', description : 'description 3' },
+    { src: '/art/art-1.webp', width : 800,  height : 1236, title : 'title 1', description : 'description 1' },
+]
