ce2d60e20e3e585258616278fa07333ed91798be
joshkaye88 <jkaye@shaw.ca>
2024-04-01 22:27:14 -0700

    complete gallery modal

--- src/lib/galleryModal.ts
+++ src/lib/galleryModal.ts
@@ -25,96 +25,35 @@ export default class GalleryModal {
         this.addImgButtonEventListeners()
     }
 
-    openModal = (i: number, e: MouseEvent) => {
+    open = (i: number, e: MouseEvent) => {
         if( this.isModalOpen() ) return
 
-        const image = this.getClickedImage(e.clientX, e.clientY)
-        if( !image ) return
+        const clickedImage = this.getClickedImage(e.clientX, e.clientY)
+        if( !clickedImage ) return
+        const clickedImageIndex = Number(clickedImage.dataset.index)
         
         this.setCurrentImageIndex(i)
-        const modalHTML  = this.generateModalHTML(image)
-        this.showModalUI(modalHTML)
+        this.showModalUI( this.ModalHTML() )
         this.setIsModalOpen(true)
-
+        this.moveToImage(clickedImageIndex, 'instant')
     }
 
-    closeModal = () => {
+    close = () => {
         this.hideModalUI()
         this.setIsModalOpen(false)
     }
         
     addImgButtonEventListeners = () => {
         const artBtnList = document.querySelectorAll(this.imgBtnElName) as NodeListOf<HTMLButtonElement>
-        artBtnList.forEach( (btn , i) => btn.addEventListener('click', (e : MouseEvent) => { this.openModal(i,e) }) )
+        artBtnList.forEach( (btn , i) => btn.addEventListener('click', (e : MouseEvent) => { this.open(i,e) }) )
     }
 
     // 
     // Open Modal Helpers
     // 
 
-    generateModalHTML = (image: HTMLImageElement) => {
-        return this.generateCloseButtonHTML() + this.generateScrollerHTML() + this.generateImageHTML()
-    }
-
-    generateCloseButtonHTML = () => {
-        return `
-            <div class="mx-auto w-full max-w-6xl flex justify-end px-2">
-                <button data-close-img-modal-button>
-                    Close
-                </button>
-            </div>
-        `
-    }
-    
-    generateScrollerHTML = () => {
-        return `
-            <div class="mx-auto w-full max-w-6xl bg-primary-50" data-img-modal-scroller>
-                <div class="my-4 pb-2 mx-4 overflow-y-scroll flex justify-start gap-2 scrollbar" data-image-content>
-                    ${imageList.map( (img, i: number) => (
-                        `<button class="w-auto min-w-fit ${i !== this.currentImageIndex ? 'opacity-50' : ''}">
-                            <img 
-                                src="${img.src}"
-                                class="h-[100px] w-auto rounded"
-                            >
-                        </button>`
-                        ))}
-                </div>
-            </div>
-        `
-
-    }
-    
-    generateImageHTML = () : string => {
-        return `
-            <div data-img-modal-content class="mx-auto w-full max-w-6xl">
-                <div class="img-modal-animation flex items-start overflow-y-scroll snap-mandatory snap-x scrollbar-hide" data-img-modal-content-scroller>
-
-                    ${ imageList.map( (image, index) => (`
-                        <div class="min-w-full grid lg:flex gap-4 lg:gap-12 items-center p-4 snap-center overflow-auto scrollbar">
-                            <img 
-                                src="${image.src}" 
-                                width="${image.width}" 
-                                height="${image.height}" 
-                                alt="" 
-                                class='rounded'
-                                style='max-height:70vh; width: auto'
-                                data-index="${index}"
-                            >
-                            <div>
-                                <h2 data-img-modal-img-title class="text-4xl font-bold font-theme">
-                                    ${image.title}
-                                </h2>
-                                <p data-img-modal-img-description>
-                                    ${image.description}
-                                </p>
-                            </div>
-                        </div>
-                    `))}
-
-                    
-                </div>
-            </div>
-        `
+    ModalHTML = () => {
+        return this.CloseButtonHTML() + this.ScrollerHTML() + this.ImageHTML()
     }
 
     addScrollerButtonEventListeners = () => {
@@ -129,12 +68,8 @@ export default class GalleryModal {
         this.modal.insertAdjacentHTML('beforeend', modalHTML)
         this.modal.classList.remove('opacity-0', '-z-50')
         this.modal.classList.add('z-[9999]')
-        document.querySelector('[data-close-img-modal-button]')?.addEventListener('click', this.closeModal)
+        document.querySelector('[data-close-img-modal-button]')?.addEventListener('click', this.close)
         this.addScrollerButtonEventListeners()
-    }
-
-    isModalOpen = () => {
-        return document.body.classList.contains('img-modal-is-open')
     }
 
     getClickedImage = (x: number, y:number) : HTMLImageElement | null => {
@@ -157,6 +92,10 @@ export default class GalleryModal {
     // 
     // Modal State
     // 
+    
+    isModalOpen = () => {
+        return document.body.classList.contains('img-modal-is-open')
+    }
 
     setIsModalOpen = (modalOpenState : boolean) => {
         document.body.classList.toggle('img-modal-is-open', modalOpenState)
@@ -170,9 +109,8 @@ export default class GalleryModal {
         this.currentImageIndex = i
     }
 
-    moveToImage = (i : number) => {
-
-        this.setScrollerOpacities(i)
+    moveToImage = (i : number, scroll : 'smooth' | 'instant' = 'smooth') => {
+        this.setScrollerImageOpacities(i)
         const imgContent = document.querySelector('[data-img-modal-content]')
         if(!imgContent) return
 
@@ -181,14 +119,14 @@ export default class GalleryModal {
         const imgScroller = document.querySelector('[data-img-modal-content-scroller]') as HTMLDivElement
         imgScroller.scrollTo({
             left : imgContentWidth * i,
-            behavior : 'smooth'
+            behavior : scroll
         })
     }
 
-    setScrollerOpacities = (i : number) => {
+    setScrollerImageOpacities = (currentImage : number) => {
         const buttons = document.querySelector('[data-img-modal-scroller]')?.querySelectorAll('button') as NodeListOf<HTMLButtonElement>
         buttons.forEach( (button, index) => { 
-            if( i === index) {
+            if( currentImage === index) {
                 button.classList.remove('opacity-50')
             } else {
                 button.classList.add('opacity-50')
@@ -196,18 +134,84 @@ export default class GalleryModal {
         })
     }
 
-    // getCurrent
+    // 
+    // Components
+    // 
+    
+    CloseButtonHTML = () => {
+        return `
+            <div class="mx-auto w-full max-w-6xl flex justify-end px-2">
+                <button class="flex items-center gap-2 px-4 py-2 rounded-md bg-primary-500 hover:bg-primary-700 text-primary-50 uppercase font-bold" data-close-img-modal-button>
+                    Close
+                    <svg width="27" height="26" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.21875 22.3507C2.97711 21.1515 1.98675 19.7171 1.30543 18.131C0.624111 16.545 0.26549 14.8391 0.250491 13.113C0.235491 11.3868 0.564413 9.675 1.21806 8.07734C1.87172 6.47969 2.83701 5.02822 4.05761 3.80761C5.27822 2.58701 6.72969 1.62172 8.32734 0.968065C9.925 0.314413 11.6368 -0.0145088 13.363 0.000490838C15.0891 0.0154904 16.795 0.374111 18.381 1.05543C19.9671 1.73675 21.4015 2.72711 22.6007 3.96875C24.9688 6.42058 26.2791 9.7044 26.2495 13.113C26.2199 16.5215 24.8527 19.7821 22.4424 22.1924C20.0321 24.6027 16.7715 25.9699 13.363 25.9995C9.9544 26.0291 6.67058 24.7188 4.21875 22.3507ZM6.05175 20.5177C8.00321 22.4692 10.65 23.5655 13.4097 23.5655C16.1695 23.5655 18.8163 22.4692 20.7677 20.5177C22.7192 18.5663 23.8155 15.9195 23.8155 13.1597C23.8155 10.4 22.7192 7.75321 20.7677 5.80175C18.8163 3.85028 16.1695 2.75396 13.4097 2.75396C10.65 2.75396 8.00321 3.85028 6.05175 5.80175C4.10028 7.75321 3.00396 10.4 3.00396 13.1597C3.00396 15.9195 4.10028 18.5663 6.05175 20.5177ZM18.9217 9.48075L15.2427 13.1597L18.9217 16.8387L17.0887 18.6717L13.4097 14.9927L9.73075 18.6717L7.89775 16.8387L11.5767 13.1597L7.89775 9.48075L9.73075 7.64775L13.4097 11.3267L17.0887 7.64775L18.9217 9.48075Z" fill="#FFE6F1"/>
+                    </svg>
+                </button>
+            </div>
+        `
+    }
+    
+    ScrollerHTML = () => {
+        return `
+            <div class="mx-auto w-full max-w-6xl bg-primary-50" data-img-modal-scroller>
+                <div class="my-4 pb-2 mx-4 overflow-y-scroll flex justify-start gap-1 scrollbar" data-image-content>
+                    ${imageList.map( (img, i: number) => (
+                        `<button class="w-auto min-w-fit ${i !== this.currentImageIndex ? 'opacity-50' : ''}">
+                            <img 
+                                src="${img.src}"
+                                class="h-[80px] w-auto rounded"
+                            >
+                        </button>`
+                        ))}
+                </div>
+            </div>
+        `
+
+    }
+    
+    ImageHTML = () : string => {
+        return `
+            <div data-img-modal-content class="mx-auto w-full max-w-6xl">
+                <div class="img-modal-animation flex items-start overflow-x-auto snap-mandatory snap-x scrollbar-hide" data-img-modal-content-scroller>
+
+                    ${ imageList.map( (image, index) => (`
+                        <div class="min-w-full grid lg:flex gap-4 lg:gap-12 items-center p-4 snap-center">
+                            <img 
+                                src="${image.src}" 
+                                width="${image.width}" 
+                                height="${image.height}" 
+                                alt="" 
+                                class='rounded'
+                                style='max-height:70vh; width: auto'
+                                data-index="${index}"
+                            >
+                            <div>
+                                <h2 data-img-modal-img-title class="text-4xl font-semibold font-theme mb-4">
+                                    ${image.title}
+                                </h2>
+                                <p data-img-modal-img-description>
+                                    ${image.description}
+                                </p>
+                            </div>
+                        </div>
+                    `))}
+
+                    
+                </div>
+            </div>
+        `
+    }
 
     // 
     // Dev Helper Methods
     // 
 
-    mockOpenModal = () => { // For Dev Use
+    mockOpenModal = () => { 
         this.currentImageIndex = 0
         const modalHTML  = 
-            this.generateCloseButtonHTML() +
-            this.generateScrollerHTML() +
-            this.generateImageHTML()
+            this.CloseButtonHTML() +
+            this.ScrollerHTML() +
+            this.ImageHTML()
 
         this.showModalUI(modalHTML)
         this.setIsModalOpen(true)
