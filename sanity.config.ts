503d880ffa69822bf3059175a94f6b403bda28b3
joshkaye88 <jkaye@shaw.ca>
2024-04-19 21:35:58 -0700

    add sanity back into project

---
+++ sanity.config.ts
@@ -0,0 +1,47 @@
+
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import { scheduledPublishing } from '@sanity/scheduled-publishing'
+import { home } from './schemaTypes/home'
+import { artwork } from './schemaTypes/artwork'
+import { bio } from './schemaTypes/bio'
+import { bloodAndThorns } from './schemaTypes/bloodAndThorns'
+import { contact } from './schemaTypes/contact'
+import { socialMediaPost } from './schemaTypes/socialMediaPost'
+
+// These are one off pages, not content lists
+// This is used to remove the Create buttons
+// See link for more info:
+// https://www.sanity.io/schemas/use-newdocumentoptions-to-configure-new-document-creation-2c640bf6
+const pages = ['home', 'bio', 'bloodAndThorns', 'contact']
+
+export default defineConfig({
+  name: 'default',
+  title: 'PinkArtWitch',
+
+  projectId: 'v81020gt',
+  dataset: 'production',
+
+  plugins: [structureTool(), scheduledPublishing()],
+
+  schema: {
+    types: [home, socialMediaPost, artwork, bloodAndThorns, bio, contact],
+  },
+
+  document: {
+    newDocumentOptions: (prev, { creationContext }) => {
+      const { type, schemaType } = creationContext
+
+      // Remove single pages from global Create button
+      if (type === 'global') { 
+        return prev.filter((template) => !pages.includes(template.templateId));
+
+      // Remove single pages' content create button
+      } else if (pages.includes(schemaType as string)) {
+        return [];
+      }
+
+      return prev;
+    },
+  },
+})
