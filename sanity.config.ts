f0e7a00e094a2650e7f5f168fd7b3fc708c3ea40
joshkaye88 <jkaye@shaw.ca>
2024-05-05 20:51:10 -0700

    minor formatting

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,7 +12,7 @@ import { media } from 'sanity-plugin-media'
 import { commonTag } from './schemaTypes/tag'
 import { support } from './schemaTypes/support'
 import { visionTool } from '@sanity/vision'
-import { orderRankField, orderRankOrdering, orderableDocumentListDeskItem } from '@sanity/orderable-document-list'
+import { orderableDocumentListDeskItem } from '@sanity/orderable-document-list'
 
 // These are one off pages, not content collections
 // This is used to remove the Create buttons
@@ -38,6 +38,8 @@ export default defineConfig({
 			return S.list()
 				.title('Content')
 				.items([
+					// The following line gets all schema:types, then filters out artwork as 
+					// orderableDocumentListDeskItem is the new sortable artwork list
 					...S.documentTypeListItems().filter(doc => doc.getId() !== 'artwork'),
 					orderableDocumentListDeskItem({ type: 'artwork', title: 'Artwork', S, context }),
 				])
@@ -52,7 +54,7 @@ export default defineConfig({
 			if (type === 'global') {
 				return prev.filter((template) => !pages.includes(template.templateId));
 
-				// Remove single pages' content create button
+			// Remove  single pages' content create button
 			} else if (pages.includes(schemaType as string)) {
 				return [];
 			}
